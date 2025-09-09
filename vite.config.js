897f971b9c5a571dbc733e63a0c5823d7540dabc
wrong-mistake <2609748897@qq.com>
2025-09-09 10:37:15 +0800

    test 1

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: '/project',
-   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+  build:{
+    outDir: 'docs'
   }
 })
